299c3896b831f5cf9401b0b10ce64c8ecb62e05d
Jason <jasonvanl2019@gmail.com>
2025-09-04 10:36:52 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ASPNET;
+
+internal class Program
+{
+    private static WebServer Server;
+
+    public static void Main(string[] args)
+    {
+        string[] prefixes = { "http://localhost:8080/" };
+        Server = new WebServer(prefixes);
+    }
+    
+    public static WebServer GetServer()
+    {
+        return Server;
+    }
+}
---
+++ WebServer.cs
@@ -0,0 +1,49 @@
+﻿using System.Net;
+
+namespace ASPNET;
+
+public class WebServer
+{
+    public static HttpListenerContext context { get; set; }
+    public static HttpListenerRequest request { get; set; }
+    public static HttpListenerResponse response { get; set; }
+
+    private string[] prefixes;
+
+    public WebServer(string[] prefixes)
+    {
+        this.prefixes = prefixes;
+        Start();
+    }
+
+    private void Start()
+    {
+        Console.WriteLine("Starting server...");
+        using var listener = new HttpListener();
+        foreach (var prefix in prefixes)
+        {
+            listener.Prefixes.Add(prefix);
+        }
+        
+        listener.Start();
+        const bool shouldListen = true;
+
+        do
+        {
+            context = listener.GetContext();
+            request = context.Request;
+            response = context.Response;
+            
+            Console.WriteLine($"Received request for {request.Url}");
+
+            var responseString = "<html><body>Hello, world!</body></html>";
+            var buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+            response.ContentLength64 = buffer.Length;
+            var output = response.OutputStream;
+            output.Write(buffer, 0, buffer.Length);
+            output.Close();
+        } while (shouldListen);
+        
+        listener.Stop();
+    }
+}
