051917b4f39fdebd84b90184c1a6515668f3ff2d
Jason <jasonvanl2019@gmail.com>
2025-09-04 11:37:54 +0200

    Added a homePage

---
+++ app/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using ASPNET.attributes;
+using ASPNET.Views;
+
+namespace ASPNET.app.Controllers;
+
+public class HomeController : Controller
+{
+    [Route("")]
+    public static string Index()
+    {
+        return new View("Home.html");
+    }
+}
